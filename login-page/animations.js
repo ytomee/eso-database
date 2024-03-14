0827722af99b181f80c76709575eff0a418282eb
Joao Teixeira <joaodanielevateixeira@gmail.com>
2024-03-14 18:20:07 +0000

    Finished layout & bug fixes
    Bugs fixed:
    - background not loading,
    - background position fixed,
    - colors,
    - shadows with the wrong color
    
    Added features:
    - loading animations,
    - new color scheme,
    - eso favicon,
    - finished global css,
    - hidden password
    
    To-do:
    - media queries,
    - change font sizes to a scale,
    - PHP

---
+++ login-page/animations.js
@@ -0,0 +1,3 @@
+gsap.from(".fade-in", {delay:.1, duration:.2, opacity: 0})
+gsap.from(".logo-load", { delay:.3, duration: 1, yPercent: 70});
+gsap.from(".input-load", { delay: .6, duration: .8, opacity: 0, yPercent: -15 });
--- login-page/index.php
+++ login-page/index.php
@@ -3,34 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <title>ESO Database</title>
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
     <!-- CSS -->
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="global-styles.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
 
-    <div id="content-wrapper">
-        <div class="box">
-            <div class="logo"></div>
+    <div id="body-wrapper" class="bg-img">
+        <div class="box shadow">
+            <div class="logo logo-load fade-in"></div>
 
             <form action="index.php" method="post">
-                <div class="input-box">
-                    <div class="input-data">
+                <div class="input-box input-load">
+                    <div class="input-data shadow">
                         <i class='bx bxs-user'></i>
                         <input name="user" type="text" placeholder="Username" required>
                     </div>
-                
-                    <div class="input-data">
+
+                    <div class="input-data shadow">
                         <i class='bx bxs-key'></i>
-                        <input name="pass" type="text" placeholder="Password" required>
+                        <input name="pass" type="password" placeholder="Password" required>
                     </div>
                 
                     <div class="submit-box">
-                        <button type="submit" class="button">
+                        <button type="submit" class="button shadow">
                             <span class="button-content">Login</span>
                         </button>
                     </div>
@@ -38,5 +39,8 @@
             </form>
         </div>
     </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.0/gsap.min.js"></script>
+    <script src="animations.js"></script>
 </body>
 </html>
